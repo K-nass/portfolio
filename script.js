ba874812ce260eea93b9a96ed23dd9c5d674dabd
karim <karimnase10@gmail.com>
2025-04-01 19:50:19 +0200

    adding resume in mobile screen and adding btn scroll to top

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const dropDown = document.querySelector("nav ul")
 const menuBtn = document.getElementById("menu_icon");
-menuBtn.addEventListener("click", function() {
+menuBtn.addEventListener("click", function () {
     dropDown.classList.toggle("show")
 })
-window.onclick = function(e) {
+window.onclick = function (e) {
     if (!menuBtn.contains(e.target) && !dropDown.contains(e.target)) {
         dropDown.classList.remove("show");
     }
 };
+
+const scrollBtn = document.getElementById("scroll_btn");
+window.addEventListener("scroll", scrollFunction);
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block"
+    } else {
+        scrollBtn.style.display = "none"
+    }
+}
+
+scrollBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
