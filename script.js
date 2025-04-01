72d015e2444c9b8d8dce94aed7054238993755c9
karim <karimnase10@gmail.com>
2025-04-01 06:40:45 +0200

    close drop down if click outside

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const menuBtn = document.getElementById("menu_icon");
 menuBtn.addEventListener("click", function() {
     dropDown.classList.toggle("show")
 })
+window.onclick = function(e) {
+    if (!menuBtn.contains(e.target) && !dropDown.contains(e.target)) {
+        dropDown.classList.remove("show");
+    }
+};
